36b125674955c6afce5c9fdcb57ce6c03113c620
csh <ayocjs@naver.com>
2022-12-22 19:47:15 +0900

    file commit

--- src/main/java/com/spring/qna/controllers/QnaController.java
+++ src/main/java/com/spring/qna/controllers/QnaController.java
@@ -8,14 +8,16 @@ import com.spring.qna.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -52,7 +54,7 @@ public class QnaController {
                         sysName,
                         dto.getQnaSeq());
                 list.add(Fdto);
-                util.save(uploadfile,qnaPath,sysName);
+                util.saves(uploadfile,qnaPath,sysName);
             }
         }
         model.addAttribute("files", list);
@@ -75,5 +77,9 @@ public class QnaController {
         Fservice.deleteFile(qnaSeq);
         util.delete(qnaPath,qnaSysName);
         return "index";
+    }
+    @RequestMapping("download")
+    public ResponseEntity<Resource> download(FileUtil util, String sysName, String oriName) throws Exception {
+        return util.download(qnaPath,sysName,oriName);
     }
 }
--- src/main/java/com/spring/qna/util/FileUtil.java
+++ src/main/java/com/spring/qna/util/FileUtil.java
@@ -4,10 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -23,7 +34,13 @@ public class FileUtil {
     String oriName;
     String sysName;
 
-    public void save(@RequestParam MultipartFile[] uploadfile, String realPath, String sysName) throws Exception {
+
+    public void save(@RequestParam MultipartFile file, String realPath, String sysName)throws Exception {
+        File fileSavePath = new File(realPath);
+        file.transferTo(new File(fileSavePath + "/" + sysName));
+    }
+
+    public void saves(@RequestParam MultipartFile[] uploadfile, String realPath, String sysName) throws Exception {
         File fileSavePath = new File(realPath);
         for (MultipartFile file : uploadfile) {
             if (!file.isEmpty()) {
@@ -35,11 +52,23 @@ public class FileUtil {
             }
         }
     }
-    public void delete(@RequestParam String realPath , String sysName) {
+
+    public void delete(@RequestParam String realPath, String sysName) {
         String fileSavePath = realPath;
         File file = new File(fileSavePath + "/" + sysName);
-        if(file.exists()){
+        if (file.exists()) {
             file.delete();
         }
+    }
+
+    public ResponseEntity<Resource> download(@ModelAttribute String realpath, String sysName ,String oriName) throws Exception {
+        Path path = Path.of(realpath + "/" + sysName);
+        String contentType = Files.probeContentType(path);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentDisposition(ContentDisposition.builder("attachment").filename(oriName, StandardCharsets.UTF_8).build());
+        headers.add(HttpHeaders.CONTENT_TYPE, contentType);
+        Resource resource = new InputStreamResource(Files.newInputStream(path));
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
     }
 }
